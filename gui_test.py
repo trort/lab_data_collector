bf12fdead4d4288d4dd0d68e297a68b2800d7430
trort <luojiajun432100@gmail.com>
2013-03-26 17:49:28 -0500

    learning wxPython

--- gui_test.py
+++ gui_test.py
@@ -1,8 +1,51 @@
-from enthought.traits.api import *
 import wx
+import os
 
-class Counter(HasTraits):
-    value =  Int()
+class MainWindow(wx.Frame):
+    """ We simply derive a new class of Frame. """
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(200,100))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.CreateStatusBar()
 
-Counter().edit_traits()
-wx.PySimpleApp().MainLoop()
+        filemenu = wx.Menu()
+
+        menuAbout = filemenu.Append(wx.ID_ABOUT, "&About", " Info about this program")
+        #self.Bind(wx.EVT_MENU, self.OnAbout, menuItem)
+        menuOpen = filemenu.Append(wx.ID_OPEN, "&Open", " Open a file to read")
+        filemenu.AppendSeparator()
+        menuExit = filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
+
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu, "&File")
+        self.SetMenuBar(menuBar)
+
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+        self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
+        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+        
+        self.Show(True)
+
+    def OnAbout(self,e):
+        dlg = wx.MessageDialog(self, "A small text editor", "About Sample Editor")
+        dlg.ShowModal()
+        dlg.Destroy()
+
+    def OnExit(self,e):
+        self.Close(True)
+
+    def OnOpen(self,e):
+        """ Open a file"""
+        self.dirname = ''
+        dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", "*.*", wx.OPEN)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.filename = dlg.GetFilename()
+            self.dirname = dlg.GetDirectory()
+            f = open(os.path.join(self.dirname, self.filename), 'r')
+            self.control.SetValue(f.read())
+            f.close()
+        dlg.Destroy()
+
+app = wx.App(False)
+frame = MainWindow(None, 'Simple editor')
+app.MainLoop()
